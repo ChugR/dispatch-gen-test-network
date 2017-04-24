52ba328893fc7cb0db4a62c7ee8fb2c858cfaae9
Chuck Rolke <crolke@redhat.com>
2017-04-24 13:17:56 -0400

    mo better

--- grep-this.sh
+++ grep-this.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 echo "Find core threads"
-find . -name "A*.txt" | xargs grep -m1 core_thread
+find . -name "A*.txt" | sort | xargs grep -m1 core_thread
 
 echo "Find worker threads"
-find . -name "A*.txt" | xargs grep -m1 process_connector | sort
+find . -name "A*.txt" | sort | xargs grep -m1 process_connector | sort
 
 echo "Find core thread top hitter"
-find . -name "A*.txt" | xargs grep -m1 -A2 core_thread
+find . -name "A*.txt" | sort | xargs grep -m1 -A2 core_thread
 
 echo "Find all delete_delivry calls"
-find . -name "A*.txt" | xargs grep -m1 -A2 delete_delivery
+find . -name "A*.txt" | sort | xargs grep -m1 -A2 delete_delivery
 
 echo "Core thread delete delivery calls"
-find . -name "A*.txt" | xargs grep -m1 -A2 delete_delivery_CT
+find . -name "A*.txt" | sort | xargs grep -m1 -A2 delete_delivery_CT
 
 echo "Specialty things we care about"
-find . -name "A*.txt" | xargs grep -m1 -A2 qd_message_free
-find . -name "A*.txt" | xargs grep -m1 -A2 qd_message_copy
+find . -name "A*.txt" | sort | xargs grep -m1 -A2 qd_message_free
+find . -name "A*.txt" | sort | xargs grep -m1 -A2 qd_message_copy
