108a37a46aac4b7db2692a35f195ba371a191c75
Chuck Rolke <crolke@redhat.com>
2017-04-25 15:20:08 -0400

    Add flamegraphs. Aggregate workers and whole router.

--- run-perf.sh
+++ run-perf.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
 # Run perf on each thread of qdrouterd process
-#  Perfs all run as subprocesses.
-#  Keeps running until you press Enter
-#  Generates perf graph reports on each thread
 #
 # $1 is the dir to use for files
+#
+#  Perfs all run as subprocesses.
+#  Keeps running until you press Enter
+#  Generates perf call graph reports on each thread
+#  Generates flamegraph (https://github.com/brendangregg/FlameGraph)
+#   reports on threads, aggregated worker threads, aggregated router.
+#  Aggregation plan:
+#
+#   core_thread -------------\
+#   worker 1 \               | ROUTER
+#   worker 2 |   AllWorkers  +----------
+#   ,,,      +---------------/
+#   worker N /
 
 # set -x
 
@@ -52,25 +62,50 @@ qdstat -l               > at-end-qdstat-l.txt
 qdstat -a               > at-end-qdstat-a.txt
 qdstat -m               > at-end-qdstat-m.txt
 
+# Create empty all-workers stack-collapse
+rm    ./ALL-STACKS.txt 2>/dev/null
+touch ./ALL-STACKS.txt
+        
 # Generate call graph perf reports
 ps -L --pid $ROUTERPID -o tid= |\
     while read tid
     do
-        perf report -g --call-graph --stdio -i A_perf_tid_$tid.data --header > A-perf-graph-tid-$tid.txt
-        # A-perf-graph-tid-$tid.txt
-        perf report -g --call-graph --stdio -i A_perf_tid_$tid.data --header > TEMP.txt
-        # Detect the core/worker threads and name reports separately
-        TYPE=work
-        if grep -q core_thread TEMP.txt; then
+        # Generate per-thread call graph as text file
+        perf report -i ./A_perf_tid_$tid.data --header -g --call-graph --stdio > ./CALL-GRAPH.txt
+
+        # Generate per-thread stack-collapse
+        perf script -i ./A_perf_tid_$tid.data | stackcollapse-perf.pl > ./STACK-COLLAPSE.txt
+
+        # Detect the core/worker threads. Concatenate all worker thread stacks
+        if grep -q core_thread CALL-GRAPH.txt; then
             TYPE=core
+        else
+            TYPE=work
+            cat ./STACK-COLLAPSE.txt >> ./ALL-STACKS.txt
         fi
+
         # Generate flamegraph
-        mv TEMP.txt A-perf-graph-$tid-$TYPE.txt
-        perf script -i A_perf_tid_$tid.data | stackcollapse-perf.pl > A_stackcollapse-perf_$tid_$TYPE.tmp
-        flamegraph.pl  A_stackcollapse-perf_$tid_$TYPE.tmp >  A_flamegraph_$tid_$TYPE.svg
-        
-        echo "Thread reports saved in A-perf-graph-tid-$tid.txt and A_flamegraph_$tid_$TYPE.svg"
+        title="$DIRECTORY - $TYPE thread $tid"
+        flamegraph.pl  --title "${title}" --width 1800 ./STACK-COLLAPSE.txt >  ./FLAMEGRAPH.svg
+
+        # Rename per-thread files to target
+        mv CALL-GRAPH.txt     A-perf-call-graph-${tid}-${TYPE}.txt
+        mv STACK-COLLAPSE.txt A-stack-collapse-${tid}-${TYPE}.txt
+        mv FLAMEGRAPH.svg     A-perf-flamegraph-${tid}-${TYPE}.svg
+
+        echo "Thread $tid reports saved in A-perf-call-graph-${tid}-${TYPE}.txt and A-perf-flamegraph-${tid}-${TYPE}.svg"
     done
+
+# Generate all-worker flamegraph
+title="$DIRECTORY - All worker threads"
+flamegraph.pl           --title "${title}" --width 1800 ./ALL-STACKS.txt > ./ALL-WORKERS.svg
+flamegraph.pl --reverse --title "${title}" --width 1800 ./ALL-STACKS.txt > ./ALL-WORKERS-reverse.svg
+
+# Generate entire router flamegraph
+title="$DIRECTORY - Whole router - all threads"
+cat ALL-STACKS.txt A-stack-collapse*-core.txt > ROUTER-stack-collapse.txt
+flamegraph.pl            --title "${title}" --width 1800 ROUTER-stack-collapse.txt > ROUTER.svg
+flamegraph.pl --reverse  --title "${title}" --width 1800 ROUTER-stack-collapse.txt > ROUTER-reverse.svg
 
 #
 popd
