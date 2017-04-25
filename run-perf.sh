254cc9c527a105685afcb536aaff0669a45cf569
Chuck Rolke <crolke@redhat.com>
2017-04-25 11:49:34 -0400

    cwip

--- run-perf.sh
+++ run-perf.sh
@@ -30,6 +30,7 @@ pushd $DIRECTORY
 top -Hbn1 -p $ROUTERPID > at-begin-top.txt
 qdstat -l               > at-begin-qdstat-l.txt
 qdstat -a               > at-begin-qdstat-a.txt
+qdstat -m               > at-begin-qdstat-m.txt
 
 # Generate perf data files per thread
 ps -L --pid $ROUTERPID -o tid= |\
@@ -49,13 +50,26 @@ killall --wait perf
 top -Hbn1 -p $ROUTERPID > at-end-top.txt
 qdstat -l               > at-end-qdstat-l.txt
 qdstat -a               > at-end-qdstat-a.txt
+qdstat -m               > at-end-qdstat-m.txt
 
 # Generate call graph perf reports
 ps -L --pid $ROUTERPID -o tid= |\
     while read tid
     do
         perf report -g --call-graph --stdio -i A_perf_tid_$tid.data --header > A-perf-graph-tid-$tid.txt
-        echo "Thread report saved in A-perf-graph-tid-$tid.txt"
+        # A-perf-graph-tid-$tid.txt
+        perf report -g --call-graph --stdio -i A_perf_tid_$tid.data --header > TEMP.txt
+        # Detect the core/worker threads and name reports separately
+        TYPE=work
+        if grep -q core_thread TEMP.txt; then
+            TYPE=core
+        fi
+        # Generate flamegraph
+        mv TEMP.txt A-perf-graph-$tid-$TYPE.txt
+        perf script -i A_perf_tid_$tid.data | stackcollapse-perf.pl > A_stackcollapse-perf_$tid_$TYPE.tmp
+        flamegraph.pl  A_stackcollapse-perf_$tid_$TYPE.tmp >  A_flamegraph_$tid_$TYPE.svg
+        
+        echo "Thread reports saved in A-perf-graph-tid-$tid.txt and A_flamegraph_$tid_$TYPE.svg"
     done
 
 #
